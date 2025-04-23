0fc4a2e60a494a2cc378d78d423d2e274cbc4178
s1895258 <s1895258@ed.ac.uk>
2025-04-23 15:54:12 +0100

    algebraic and arithmetic floating-point kernel templates for manual unroll (factor=8 to fit 8x64bit burst reads/writes)

--- generate/kernel_template/floating_point/algebraic.py
+++ generate/kernel_template/floating_point/algebraic.py
@@ -36,14 +36,8 @@ struct packed_data {{
   HOST_DTYPE data[EXTERNAL_DATA_WIDTH];
 }};
 
-template<typename DT>
-static DT bench(DT val1);
-static void retrieve_data(struct packed_data*, hls::stream<DEVICE_DTYPE>&, int);
-static void do_compute(hls::stream<DEVICE_DTYPE>&, hls::stream<DEVICE_DTYPE>&, int);
-static void write_data(hls::stream<DEVICE_DTYPE>&, struct packed_data*, int);
-
 extern "C" {{
-void krnl_bench(struct packed_data * val1, struct packed_data * result, int num_its) {{
+void krnl_bench(struct packed_data * val1, struct_packed_data * result, int num_its) {{
 #pragma HLS INTERFACE m_axi port=val1 offset=slave bundle=gmem
 #pragma HLS INTERFACE m_axi port=result offset=slave bundle=gmem
 
@@ -52,68 +46,24 @@ void krnl_bench(struct packed_data * val1, struct packed_data * result, int num_
 #pragma HLS INTERFACE s_axilite port=num_its bundle=control
 #pragma HLS INTERFACE s_axilite port=return bundle=control
 
-  hls::stream<DEVICE_DTYPE> val1_stream, result_stream;
-#pragma HLS STREAM variable=val1_stream depth=8
-#pragma HLS STREAM variable=result_stream depth=8
-
-#pragma HLS DATAFLOW
-  retrieve_data(val1, val1_stream, num_its);
-  do_compute(val1_stream, result_stream, num_its);
-  write_data(result_stream, result, num_its);
-}}
-}}
-
-static void retrieve_data(struct packed_data * val1, hls::stream<DEVICE_DTYPE> & stream, int num_its) {{
-  const unsigned int base_its=num_its / EXTERNAL_DATA_WIDTH;
-  main_load_data_loop:
-  for (unsigned int i=0;i<base_its;i++) {{
-PRAGMA(HLS PIPELINE II=EXTERNAL_DATA_WIDTH)
-    struct packed_data real_values=val1[i];
-    for (unsigned int j=0;j<EXTERNAL_DATA_WIDTH;j++) stream.write((DEVICE_DTYPE) real_values.data[j]);
-  }}
-  if (base_its * EXTERNAL_DATA_WIDTH < num_its) {{
-    struct packed_data real_values=val1[base_its];
-    remainder_load_data_loop:
-    for (unsigned int j=0;j<num_its - (base_its * EXTERNAL_DATA_WIDTH);j++) stream.write((DEVICE_DTYPE) real_values.data[j]);
-  }}
-}}
-
-static void do_compute(hls::stream<DEVICE_DTYPE> & val1_stream, hls::stream<DEVICE_DTYPE> & output_stream, int num_its) {{
-  main_compute_loop:
+main_compute_loop:
   for(unsigned int i=0;i<num_its;i++) {{
-#pragma HLS PIPELINE II=1
-    DEVICE_DTYPE in1 = val1_stream.read();
-    DEVICE_DTYPE out = bench<DEVICE_DTYPE>(in1);
-    output_stream.write(out);
+//#pragma HLS UNROLL factor=1000
+    struct packed_data v1 = val1[i];
+    struct packed_data res;
+
+#pragma HLS BIND_OP variable=res op={op} impl={impl}
+    res.data[0] = (HOST_DTYPE)(hls::{func}((DEVICE_DTYPE)v1.data[0]));
+    res.data[1] = (HOST_DTYPE)(hls::{func}((DEVICE_DTYPE)v1.data[1]));
+    res.data[2] = (HOST_DTYPE)(hls::{func}((DEVICE_DTYPE)v1.data[2]));
+    res.data[3] = (HOST_DTYPE)(hls::{func}((DEVICE_DTYPE)v1.data[3]));
+    res.data[4] = (HOST_DTYPE)(hls::{func}((DEVICE_DTYPE)v1.data[4]));
+    res.data[5] = (HOST_DTYPE)(hls::{func}((DEVICE_DTYPE)v1.data[5]));
+    res.data[6] = (HOST_DTYPE)(hls::{func}((DEVICE_DTYPE)v1.data[6]));
+    res.data[7] = (HOST_DTYPE)(hls::{func}((DEVICE_DTYPE)v1.data[7]));
+
+    result[i] = res; 
   }}
 }}
-
-static void write_data(hls::stream<DEVICE_DTYPE> & result_stream, struct packed_data * out, int num_its) {{
-  const unsigned int base_its=num_its / EXTERNAL_DATA_WIDTH;
-  main_write_data_loop:
-  for (unsigned int i=0;i<base_its;i++) {{
-PRAGMA(HLS PIPELINE II=EXTERNAL_DATA_WIDTH)
-    struct packed_data real_values;
-    for (unsigned int j=0;j<EXTERNAL_DATA_WIDTH;j++) {{
-      real_values.data[j]=(HOST_DTYPE) result_stream.read();
-    }}
-    out[i]=real_values;
-  }}
-  if (base_its * EXTERNAL_DATA_WIDTH < num_its) {{
-    struct packed_data real_values;
-    remainder_write_data_loop:
-    for (unsigned int j=0;j<num_its - (base_its * EXTERNAL_DATA_WIDTH);j++) {{
-      real_values.data[j]=(HOST_DTYPE) result_stream.read();
-    }}
-    out[base_its]=real_values;
-  }}
-}}
-
-template<typename DT>
-static DT bench(DT val1) {{
-  DT out;
-#pragma HLS BIND_OP variable=out op={op} impl={impl}
-  out = hls::{func}(val1);
-  return out;
 }}
                """
--- generate/kernel_template/floating_point/arithmetic.py
+++ generate/kernel_template/floating_point/arithmetic.py
@@ -34,14 +34,8 @@ typedef double HOST_DTYPE;
 
 // The data structure used to pack external memory accesses
 struct packed_data {{
-  DEVICE_DTYPE data[EXTERNAL_DATA_WIDTH];
+  HOST_DTYPE data[EXTERNAL_DATA_WIDTH];
 }};
-
-template<typename DT>
-static DT bench(DT val1, DT val2);
-static void retrieve_data(struct packed_data*, hls::stream<DEVICE_DTYPE>&, int);
-static void do_compute(hls::stream<DEVICE_DTYPE>&, hls::stream<DEVICE_DTYPE>&, hls::stream<DEVICE_DTYPE>&, int);
-static void write_data(hls::stream<DEVICE_DTYPE>&, struct packed_data*, int);
 
 extern "C" {{
 void krnl_bench(struct packed_data * val1, struct packed_data * val2, struct packed_data * result, int num_its) {{
@@ -55,71 +49,25 @@ void krnl_bench(struct packed_data * val1, struct packed_data * val2, struct pac
 #pragma HLS INTERFACE s_axilite port=num_its bundle=control
 #pragma HLS INTERFACE s_axilite port=return bundle=control
 
-  hls::stream<DEVICE_DTYPE> val1_stream, val2_stream, result_stream;
-#pragma HLS STREAM variable=val1_stream depth=8
-#pragma HLS STREAM variable=val2_stream depth=8
-#pragma HLS STREAM variable=result_stream depth=8
-
-#pragma HLS DATAFLOW
-  retrieve_data(val1, val1_stream, num_its);
-  retrieve_data(val2, val2_stream, num_its);
-  do_compute(val1_stream, val2_stream, result_stream, num_its);
-  write_data(result_stream, result, num_its);
-}}
-}}
-
-static void retrieve_data(struct packed_data * val1, hls::stream<DEVICE_DTYPE> & stream, int num_its) {{
-  const unsigned int base_its=num_its / EXTERNAL_DATA_WIDTH;
-  main_load_data_loop:
-  for (unsigned int i=0;i<base_its;i++) {{
-PRAGMA(HLS PIPELINE II=EXTERNAL_DATA_WIDTH)
-    struct packed_data real_values=val1[i];
-    for (unsigned int j=0;j<EXTERNAL_DATA_WIDTH;j++) stream.write((DEVICE_DTYPE) real_values.data[j]);
-  }}
-  if (base_its * EXTERNAL_DATA_WIDTH < num_its) {{
-    struct packed_data real_values=val1[base_its];
-    remainder_load_data_loop:
-    for (unsigned int j=0;j<num_its - (base_its * EXTERNAL_DATA_WIDTH);j++) stream.write((DEVICE_DTYPE) real_values.data[j]);
-  }}
-}}
-
-static void do_compute(hls::stream<DEVICE_DTYPE> & val1_stream, hls::stream<DEVICE_DTYPE> & val2_stream, hls::stream<DEVICE_DTYPE> & output_stream, int num_its) {{
   main_compute_loop:
   for(unsigned int i=0;i<num_its;i++) {{
-#pragma HLS PIPELINE II=1
-    DEVICE_DTYPE in1 = val1_stream.read();
-    DEVICE_DTYPE in2 = val2_stream.read();
-    DEVICE_DTYPE out = bench<DEVICE_DTYPE>(in1, in2);
-    output_stream.write(out);
+//#pragma HLS UNROLL factor=1000
+    struct packed_data v1 = val1[i];
+    struct packed_data v2 = val2[i];
+    struct packed_data res;
+
+#pragma HLS BIND_OP variable=res op={op} impl={impl} latency=0
+    res.data[0] = (HOST_DTYPE)((DEVICE_DTYPE)v1.data[0] {sign} (DEVICE_DTYPE)v2.data[0]);
+    res.data[1] = (HOST_DTYPE)((DEVICE_DTYPE)v1.data[1] {sign} (DEVICE_DTYPE)v2.data[1]);
+    res.data[2] = (HOST_DTYPE)((DEVICE_DTYPE)v1.data[2] {sign} (DEVICE_DTYPE)v2.data[2]);
+    res.data[3] = (HOST_DTYPE)((DEVICE_DTYPE)v1.data[3] {sign} (DEVICE_DTYPE)v2.data[3]);
+    res.data[4] = (HOST_DTYPE)((DEVICE_DTYPE)v1.data[4] {sign} (DEVICE_DTYPE)v2.data[4]);
+    res.data[5] = (HOST_DTYPE)((DEVICE_DTYPE)v1.data[5] {sign} (DEVICE_DTYPE)v2.data[5]);
+    res.data[6] = (HOST_DTYPE)((DEVICE_DTYPE)v1.data[6] {sign} (DEVICE_DTYPE)v2.data[6]);
+    res.data[7] = (HOST_DTYPE)((DEVICE_DTYPE)v1.data[7] {sign} (DEVICE_DTYPE)v2.data[7]);
+
+    result[i] = res;
   }}
 }}
-
-static void write_data(hls::stream<DEVICE_DTYPE> & result_stream, struct packed_data * out, int num_its) {{
-  const unsigned int base_its=num_its / EXTERNAL_DATA_WIDTH;
-  main_write_data_loop:
-  for (unsigned int i=0;i<base_its;i++) {{
-PRAGMA(HLS PIPELINE II=EXTERNAL_DATA_WIDTH)
-    struct packed_data real_values;
-    for (unsigned int j=0;j<EXTERNAL_DATA_WIDTH;j++) {{
-      real_values.data[j]=(HOST_DTYPE) result_stream.read();
-    }}
-    out[i]=real_values;
-  }}
-  if (base_its * EXTERNAL_DATA_WIDTH < num_its) {{
-    struct packed_data real_values;
-    remainder_write_data_loop:
-    for (unsigned int j=0;j<num_its - (base_its * EXTERNAL_DATA_WIDTH);j++) {{
-      real_values.data[j]=(HOST_DTYPE) result_stream.read();
-    }}
-    out[base_its]=real_values;
-  }}
-}}
-
-template<typename DT>
-static DT bench(DT val1, DT val2) {{
-  DT out;
-#pragma HLS BIND_OP variable=out op={op} impl={impl}
-  out = val1 {sign} val2;
-  return out;
 }}
                """
